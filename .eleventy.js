18bc8aaa92b1ba8aa2400e647920016e19c034f4
karel <karel.de.smet@outlook.com>
2020-02-20 19:40:51 +0100

    Rewrote part of the articles and fixed a bug regarding 404 errors while loading favicons

--- .eleventy.js
+++ .eleventy.js
@@ -3,7 +3,7 @@ module.exports = function(eleventyConfig) {
 
   eleventyConfig.addNunjucksFilter("date", function(date) {
     return (
-      date.getDate() + "-" + (date.getMonth() + 1) + "-" + date.getFullYear()
+      date.getDate() + "/" + (date.getMonth() + 1) + "/" + date.getFullYear()
     );
   });
 
