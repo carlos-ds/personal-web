c06dd979210e6e4fbf36065fc890e0127b59afba
Karel De Smet <karel.de.smet@outlook.com>
2021-04-13 21:41:57 +0200

    Change article date format so it matches the <time> tag requirements

--- .eleventy.js
+++ .eleventy.js
@@ -4,7 +4,11 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy('assets');
 
   eleventyConfig.addNunjucksFilter('date', function (date) {
-    return date.getDate() + '/' + (date.getMonth() + 1) + '/' + date.getFullYear();
+    const day = date.getDate() > 9 ? date.getDate() : '0' + date.getDate();
+    const month = date.getMonth() > 8 ? date.getMonth() + 1 : '0' + (date.getMonth() + 1);
+    const year = date.getFullYear();
+
+    return `${year}-${month}-${day}`;
   });
 
   return {
