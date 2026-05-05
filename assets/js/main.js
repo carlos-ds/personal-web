bdafb8fc8a896a3d5b5fa0563cdeb7c127c1803d
Karel De Smet <karel.de.smet@outlook.com>
2026-05-05 06:09:15 +0200

    Toggle aria-hidden attribute for assistive technologies on mobile devices + refactor main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,25 @@
-let isNavigationClosed = true;
+// The value of this variable should remain aligned with the media query in CSS
+const MOBILE_BREAKPOINT = 768;
+
+let isNavigationHidden = true;
 
 document.addEventListener("DOMContentLoaded", () => {
+  toggleNavigation();
+});
+
+function toggleNavigation() {
   const hamburgerIcon = document.getElementById("hamburger-icon");
+  const navItems = document.querySelector(".nav-items");
+
+  toggleAttributesForAssistiveTechnologiesOnMobileNavigation(navItems, true);
 
   hamburgerIcon.addEventListener("click", () => {
-    const navItems = document.querySelector(".nav-items");
+    toggleAttributesForAssistiveTechnologiesOnMobileNavigation(
+      navItems,
+      !isNavigationHidden,
+    );
 
-    if (isNavigationClosed) {
+    if (isNavigationHidden) {
       navItems.classList.add("opaque");
       navItems.classList.add("height--auto");
     } else {
@@ -15,8 +28,23 @@ document.addEventListener("DOMContentLoaded", () => {
     }
 
     const icon = document.querySelector(".material-symbols-outlined");
-    icon.textContent = isNavigationClosed ? "close" : "menu";
+    icon.textContent = isNavigationHidden ? "close" : "menu";
 
-    isNavigationClosed = !isNavigationClosed;
+    isNavigationHidden = !isNavigationHidden;
   });
-});
+}
+
+function toggleAttributesForAssistiveTechnologiesOnMobileNavigation(
+  navItems,
+  isHidden,
+) {
+  const viewportWidth = window.innerWidth;
+
+  if (viewportWidth < MOBILE_BREAKPOINT) {
+    if (isHidden) {
+      navItems.setAttribute("aria-hidden", true);
+    } else {
+      navItems.removeAttribute("aria-hidden");
+    }
+  }
+}
